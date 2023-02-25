488b1d5b3c39d7f80b9353019015108d6e873564
admin <nguyen.nguyenvan@finhay.com.vn>
2023-02-26 03:05:36 +0700

    add swift package

---
+++ CMVP-Package/Sources/CMVP-Package/CMVP_Package.swift
@@ -0,0 +1,6 @@
+public struct CMVP_Package {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ CMVP-Package/Tests/CMVP-PackageTests/CMVP_PackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CMVP_Package
+
+final class CMVP_PackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CMVP_Package().text, "Hello, World!")
+    }
+}
